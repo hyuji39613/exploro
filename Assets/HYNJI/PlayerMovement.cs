cef090b144ad63ff00b8ca4ad795bb1344c1b052
parasife <ij0047846@gmail.com>
2024-04-04 20:27:29 +0900

    Jump, Camera

--- Assets/HYNJI/PlayerMovement.cs
+++ Assets/HYNJI/PlayerMovement.cs
@@ -1,30 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 moveDir;
+    Vector2 moveDir = Vector2.zero;
+    public float jumpScale = 5f;
     public float speed;
     private bool canJump = true;
-    private Rigidbody2D rigid;
-    [SerializeField] private float jumpHeight;
-    private void Start()
+    private Rigidbody2D rb;
+
+    void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        moveDir = new Vector3(h, v, 0);
-        transform.position += moveDir * speed * Time.deltaTime;
+        moveDir = new Vector2(h, 0);
+        transform.position += (Vector3)moveDir * speed * Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            
+            canJump = false;
+            rb.AddForce(Vector2.up * jumpScale, ForceMode2D.Impulse);
+            Invoke(nameof(SetCanJump), 3);
         }
+    }
 
-    }  
+    private void SetCanJump()
+    {
+        canJump = true;
+    }
 }
