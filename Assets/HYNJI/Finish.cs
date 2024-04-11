08a9ecc65bcc52a72d8e27a5c4ac40497444d232
hyuji39613 <ji705057@gmail.com>
2024-04-11 18:39:30 +0900

    Finish Point

---
+++ Assets/HYNJI/Finish.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        { 
+            Debug.Log("Stage2");
+            SceneManager.LoadScene("Stage2");
+        }
+    }
+}
+
