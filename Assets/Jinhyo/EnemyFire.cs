71f256507e5759e54737aff4d90a61d48fe3d44b
parasife <ij0047846@gmail.com>
2024-04-11 19:22:08 +0900

    1

--- Assets/Jinhyo/EnemyFire.cs
+++ Assets/Jinhyo/EnemyFire.cs
@@ -6,10 +6,11 @@ public class EnemyFire : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject firePos;
+    public float ThrowDelay = 1;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating(nameof(Fire), 1, 1);
+        InvokeRepeating(nameof(Fire), ThrowDelay, ThrowDelay);
     }
 
     
