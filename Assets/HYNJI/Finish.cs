72f066a8997a1c3adf98c18c1de41483a41ca642
parasife <ij0047846@gmail.com>
2024-04-24 20:05:45 +0900

    도착 수정

--- Assets/HYNJI/Finish.cs
+++ Assets/HYNJI/Finish.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    public string scName;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         { 
             Debug.Log("Stage2");
-            SceneManager.LoadScene("Stage2");
+            SceneManager.LoadScene(scName);
         }
     }
 }
