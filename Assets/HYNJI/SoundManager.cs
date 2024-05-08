a2988a3cc3d03430020f88ef1f9f4118f6ced79b
hyuji39613 <ji705057@gmail.com>
2024-05-08 21:56:49 +0900

    Sound

---
+++ Assets/HYNJI/SoundManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource bgm;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        bgm.Play();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
