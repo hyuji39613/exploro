6edcc4f75a0d7b100dd9ca09e229457ef8e0b78d
parasife <ij0047846@gmail.com>
2024-05-09 18:31:25 +0900

    수정

--- Assets/Jinhyo/Boss.cs
+++ Assets/Jinhyo/Boss.cs
@@ -11,15 +11,20 @@ public class Boss : MonoBehaviour
     public GameObject bulletfrepab; 
     public GameObject bulletfrepab2;
     public GameObject timeUi;
+    private Animator animator;
     Vector2 dir;
     float cuTime = 0;
     float creTime = 1;
     float cuTime2 = 0;
     float creTime2 = 1;
+    float dieTime = 0;
+    bool isDie = false;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spRen = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
     }
     private void Start()
     {
@@ -27,12 +32,13 @@ public class Boss : MonoBehaviour
     }
     void Update()
     {
-        if (pl.transform.position.y <= -7)
+        if (pl.transform.position.y <= -7&& !isDie)
         {
             timeUi.SetActive(true);
             rigid.gravityScale = 1;
             cuTime += Time.deltaTime;
             cuTime2 += Time.deltaTime;
+            dieTime += Time.deltaTime;
             if (cuTime > creTime)
             {
                 cuTime = 0;
@@ -46,6 +52,12 @@ public class Boss : MonoBehaviour
                 GameObject bullet2 = Instantiate(bulletfrepab2);
                 bullet2.transform.position = BossFirePos.transform.position;
                 creTime2 = UnityEngine.Random.Range(1, 3);
+            }
+            if (dieTime >= 27.5f)
+            {
+                animator.SetBool("Dead",true);
+                isDie = true;
+                Invoke(nameof(destory), 2.5f);
             }
         }
         if (pl.transform.position.x > transform.position.x)
@@ -62,6 +74,15 @@ public class Boss : MonoBehaviour
             dir = (pl.transform.position - transform.position).normalized;
             dir.y = 0;
             rigid.AddForce(dir * Time.deltaTime * 5, ForceMode2D.Impulse);
+            animator.SetBool("IsJump", true);
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetBool("IsJump", false);
+    }
+    private void destory()
+    {
+        gameObject.SetActive(false);   
     }
 }
